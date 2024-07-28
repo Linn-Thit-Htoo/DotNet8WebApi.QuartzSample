e3e888ad3a5874a4fda51c5594846c525321971e
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-28 22:20:59 +0630

    Add project files.

---
+++ DotNet8WebApi.QuartzSample/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using Quartz;
+
+namespace DotNet8WebApi.QuartzSample
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddDependencyInjection(this IServiceCollection services)
+        {
+            services.AddQuartz(opt =>
+            {
+                var jobKey = JobKey.Create(nameof(SampleBackgroundJob));
+
+                opt.AddJob<SampleBackgroundJob>(jobKey)
+                    .AddTrigger(opt =>
+                        opt.ForJob(jobKey)
+                            .WithSimpleSchedule(opt => opt.WithIntervalInSeconds(5).RepeatForever())
+                    );
+            });
+
+            services.AddQuartzHostedService(opt =>
+            {
+                opt.WaitForJobsToComplete = true;
+            });
+
+            return services;
+        }
+    }
+}
---
+++ DotNet8WebApi.QuartzSample/SampleBackgroundJob.cs
@@ -0,0 +1,20 @@
+﻿using Quartz;
+
+namespace DotNet8WebApi.QuartzSample
+{
+    public class SampleBackgroundJob : IJob
+    {
+        private readonly ILogger<SampleBackgroundJob> _logger;
+
+        public SampleBackgroundJob(ILogger<SampleBackgroundJob> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Execute(IJobExecutionContext context)
+        {
+            _logger.LogInformation("Hello world!");
+            return Task.CompletedTask;
+        }
+    }
+}
