d5e53360be57c3906415379d07ea80c0599e8e94
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-28 22:24:39 +0630

    -

--- DotNet8WebApi.QuartzSample/DependencyInjection.cs
+++ DotNet8WebApi.QuartzSample/DependencyInjection.cs
@@ -1,28 +1,27 @@
 ﻿using Quartz;
 
-namespace DotNet8WebApi.QuartzSample
+namespace DotNet8WebApi.QuartzSample;
+
+public static class DependencyInjection
 {
-    public static class DependencyInjection
+    public static IServiceCollection AddDependencyInjection(this IServiceCollection services)
     {
-        public static IServiceCollection AddDependencyInjection(this IServiceCollection services)
+        services.AddQuartz(opt =>
         {
-            services.AddQuartz(opt =>
-            {
-                var jobKey = JobKey.Create(nameof(SampleBackgroundJob));
+            var jobKey = JobKey.Create(nameof(SampleBackgroundJob));
 
-                opt.AddJob<SampleBackgroundJob>(jobKey)
-                    .AddTrigger(opt =>
-                        opt.ForJob(jobKey)
-                            .WithSimpleSchedule(opt => opt.WithIntervalInSeconds(5).RepeatForever())
-                    );
-            });
+            opt.AddJob<SampleBackgroundJob>(jobKey)
+                .AddTrigger(opt =>
+                    opt.ForJob(jobKey)
+                        .WithSimpleSchedule(opt => opt.WithIntervalInSeconds(5).RepeatForever())
+                );
+        });
 
-            services.AddQuartzHostedService(opt =>
-            {
-                opt.WaitForJobsToComplete = true;
-            });
+        services.AddQuartzHostedService(opt =>
+        {
+            opt.WaitForJobsToComplete = true;
+        });
 
-            return services;
-        }
+        return services;
     }
 }
